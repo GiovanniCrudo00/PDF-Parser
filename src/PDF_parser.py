81b22dbaae9dda16fcd8981ba01a2d001582cba5
GiovanniCrudo00 <crugio23@gmail.com>
2023-02-15 19:41:11 +0100

    Generated PDF

--- src/PDF_parser.py
+++ src/PDF_parser.py
@@ -7,18 +7,31 @@ from datetime import datetime
 # Prendo i dati da una query o un file di testo
 #
 
-my_name = "Frank Andrade"
-item1 = "TV"
-item2 = "Couch"
-item3 = "Washing Machine"
+lettera_di_vettura = "445"
 today_date = datetime.today().strftime("%d %b, %Y")
-
+mittente_nome = "DFG Servizi s.r.l."
+mittente_via = "Via Marcellinara s.n.c."
+mittente_luogo = "Marcellinara, Catanzaro, 88100"
+destinatario_nome = "LORY EXPRESS S.N.C."
+destinatario_via = "Via Montepalma, 1"
+destinatario_luogo = "95125 Catania CT"
+contrassegno = "Tipica stringa del contrassegno non lo so"
+annotazioni= "Non è arrivato niente"
+data_inizio_trasporto = "23/09/2023"
+numero_colli = "2"
+peso = "2kg"
+ddt="5"
 #
 # Assegno ai placeholder un valore dai dati recuperati
 #
 
-context = {'my_name': my_name, 'item1': item1, 'item2': item2, 'item3': item3,
-           'today_date': today_date}
+context = {'lettera_di_vettura': lettera_di_vettura, 'mittente_nome': mittente_nome, 
+            'mittente_via': mittente_via, 'mittente_luogo': mittente_luogo,
+            'destinatario_nome':destinatario_nome,'destinatario_via':destinatario_via,
+            'destinatario_luogo':destinatario_luogo, 'contrassegno':contrassegno,
+            'data_inizio_trasporto':data_inizio_trasporto, 'numero_colli':numero_colli,
+            'peso':peso,
+           'data': today_date, 'annotazioni':annotazioni, 'DDT':ddt}
 
 #
 # Genero il PDF dando il template, il path della libreria wkhtmltopdf e il nome
@@ -27,7 +40,7 @@ context = {'my_name': my_name, 'item1': item1, 'item2': item2, 'item3': item3,
 template_loader = jinja2.FileSystemLoader('./')
 template_env = jinja2.Environment(loader=template_loader)
 
-html_template = './templates/template.html'
+html_template = './templates/bollaTemplate.html'
 template = template_env.get_template(html_template)
 output_text = template.render(context)
 
