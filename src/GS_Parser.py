2dd5a9656088ca5a00d4f43bf54587984d204905
GiovanniCrudo00 <crugio23@gmail.com>
2023-02-26 20:16:40 +0100

    Structure Changes

---
+++ src/GS_Parser.py
@@ -0,0 +1,73 @@
+import subprocess
+import jinja2
+import pdfkit
+from datetime import datetime
+from ReadFromDb import read_from_db
+
+
+#
+# Prevelo il path assoluto della macchina
+#
+
+output = subprocess.check_output(['pwd'])
+str_abs = output.decode('utf8', errors='strict').strip() # Path da concatenare alla cartella delle immagini
+output = subprocess.check_output(['which','wkhtmltopdf'])
+str_wkhtmltopdf = output.decode('utf8', errors='strict').strip() # Path della libreria wkhtmltopdf
+
+azienda = "General_Service" # Selezionatore azienda
+
+#
+# Popolo i campi per la creazione del pdf
+#
+
+data_nome_pdf = datetime.today().strftime("%Y-%m-%d")
+today_date = datetime.today().strftime("%d %b, %Y")
+#
+# Modificare i path qui sotto con i nomi delle risorse necessarie
+#
+#
+logo=str_abs+"/images/GeneralService_logo_intestato.jpeg"
+html_template = str_abs+'/templates/GeneralServiceTemplate.html'
+
+#
+# Prelevo da DB le spedizioni
+#
+
+# Ottengo una lista in cui ogni elemento è una lista che rappresenta la spedizione
+lista_delle_spedizioni=read_from_db() 
+
+#
+# Genero un PDF per ogni spedizione
+#
+
+doc_id=0
+for i in lista_delle_spedizioni:
+    lettera_di_vettura = i[0]
+    mittente_nome = i[1]
+    mittente_via = i[2]
+    mittente_luogo = i[3]
+    destinatario_nome = i[4]
+    destinatario_via = i[5]
+    destinatario_luogo = i[6]
+    contrassegno = i[7]
+    annotazioni= i[8]
+    numero_colli = i[9]
+    peso = i[10]
+    ddt=i[11]
+
+    context = {'logo':logo,'lettera_di_vettura': lettera_di_vettura, 'mittente_nome': mittente_nome, 
+                'mittente_via': mittente_via, 'mittente_luogo': mittente_luogo,
+                'destinatario_nome':destinatario_nome,'destinatario_via':destinatario_via,
+                'destinatario_luogo':destinatario_luogo, 'contrassegno':contrassegno,
+                'numero_colli':numero_colli, 'peso':peso, 'data': today_date, 
+                'annotazioni':annotazioni, 'DDT':ddt}
+
+    template_loader = jinja2.FileSystemLoader('/')
+    template_env = jinja2.Environment(loader=template_loader)
+    template = template_env.get_template(html_template)
+    output_text = template.render(context)
+    config = pdfkit.configuration(wkhtmltopdf=str_wkhtmltopdf)
+    output_pdf = str_abs+'/output/'+str(data_nome_pdf)+'_'+str(lettera_di_vettura)+'.pdf'
+    options={"enable-local-file-access": ""}
+    pdfkit.from_string(output_text, output_pdf, configuration=config, options=options)
+    doc_id+=1
--- src/ReadFromDb.py
+++ src/ReadFromDb.py
@@ -1,6 +1,6 @@
 lstt = []
 # DATI DI DEBUG #
-
+"""
 lstt.append(['445','DFG Servizi SRL','Via Corace 27', 'Catanzaro, 88100','LORY EXPRESS S.N.C.','Via Montepalma, 1','95125 Catania CT','150','Nessuna annotazione','4','40','19'])
 lstt.append(['1990','DFG Servizi SRL','Via Corace 27', 'Catanzaro, 88100','Talmone Gift S.R.L','Via Jannelli, 15','Catanzaro 88100','470','Manca pacchetto','5','50','9981'])
 lstt.append(['27110','DFG Servizi SRL','Via Corace 27','Catanzaro, 88100','Talmone Gift S.R.L','Via Jannelli, 15','Catanzaro 88100','470','Pacchetto trovato','1','100','881'])
@@ -21,7 +21,7 @@ lstt.append(['2112313','DFG Servizi SRL','Via Corace 27', 'Catanzaro, 88100','LO
 lstt.append(['900','DFG Servizi SRL','Via Corace 27', 'Catanzaro, 88100','Talmone Gift S.R.L','Via Jannelli, 15','Catanzaro 88100','470','Manca pacchetto','5','50','9981'])
 lstt.append(['6616','DFG Servizi SRL','Via Corace 27','Catanzaro, 88100','Talmone Gift S.R.L','Via Jannelli, 15','Catanzaro 88100','470','Pacchetto trovato','1','100','881'])
 lstt.append(['7789','DFG Servizi SRL','Via Corace 27','Catanzaro 88100','Coop SPA','Via Barone, 159','Catanzaro 88100','4500','-','10','110','34'])
-
+"""
 #
 # Far ritornare la lista dopo averla presa dal DB
 #
