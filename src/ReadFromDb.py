7c50330489888c6212302352b249bde3a29cfbf5
GiovanniCrudo00 <crugio23@gmail.com>
2023-02-26 20:25:48 +0100

    Ready for GS

--- src/ReadFromDb.py
+++ src/ReadFromDb.py
@@ -1,8 +1,9 @@
 lstt = []
 # DATI DI DEBUG #
-"""
+
 lstt.append(['445','DFG Servizi SRL','Via Corace 27', 'Catanzaro, 88100','LORY EXPRESS S.N.C.','Via Montepalma, 1','95125 Catania CT','150','Nessuna annotazione','4','40','19'])
 lstt.append(['1990','DFG Servizi SRL','Via Corace 27', 'Catanzaro, 88100','Talmone Gift S.R.L','Via Jannelli, 15','Catanzaro 88100','470','Manca pacchetto','5','50','9981'])
+"""
 lstt.append(['27110','DFG Servizi SRL','Via Corace 27','Catanzaro, 88100','Talmone Gift S.R.L','Via Jannelli, 15','Catanzaro 88100','470','Pacchetto trovato','1','100','881'])
 lstt.append(['444','DFG Servizi SRL','Via Corace 27','Catanzaro 88100','Coop SPA','Via Barone, 159','Catanzaro 88100','4500','-','10','110','34'])
 lstt.append(['8188','DFG Servizi SRL','Via Corace 27', 'Catanzaro, 88100','LORY EXPRESS S.N.C.','Via Montepalma, 1','95125 Catania CT','150','Nessuna annotazione','4','40','19'])
