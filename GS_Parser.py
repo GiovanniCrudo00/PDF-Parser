266368c54efd93480b7791d3bae9ae8f5769bbff
GiovanniCrudo00 <crugio23@gmail.com>
2023-03-08 21:42:51 +0100

    Bordero OK manca spaziatura

--- GS_Parser.py
+++ GS_Parser.py
@@ -2,7 +2,7 @@ import subprocess
 import jinja2
 import pdfkit
 from datetime import datetime
-from src.ReadFromDb import read_from_db
+from src.ReadFromDb import read_from_db, numero_spedizioni
 
 
 #
@@ -33,9 +33,15 @@ bordero_template = str_abs+'/templates/bordero.html'
 #
 # Prelevo da DB le spedizioni
 #
-
+# In questa sezione mi aspetto che i dati mi vengano forniti nel seguente modo:
+#   Una lista di spedizioni, una lista per ogni corriere
+#   Un intero che indica il numero di spedizioni di ogni corriere
+#       cioè il numero di spedizioni che hanno il destinatario diverso.
+#
 # Ottengo una lista in cui ogni elemento è una lista che rappresenta la spedizione
 lista_delle_spedizioni=read_from_db() 
+numero_sped = numero_spedizioni()
+
 
 # Inizializzo il file html del borderò
 newHtmlFile = open('templates/bordero.html', 'w')
@@ -47,33 +53,33 @@ newHtmlFile.write("""\t\t<meta name="viewport" content="width=device-width, init
 newHtmlFile.write("""\t\t<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">\n""")
 newHtmlFile.write("""\t</head>\n""")
 newHtmlFile.write("""\t<body>\n""")
-newHtmlFile.write("""\t\t<div class="container">\n""")
+newHtmlFile.write("""\t\t<div class="container-fluid">\n""")
 newHtmlFile.write("""\t\t\t<div class="row">\n""")
 newHtmlFile.write("""\t\t\t\t<div class="col-sm" style="text-align: left;"> <img style="height: 120px;" src={{logo}}></div>\n""")
 newHtmlFile.write("""\t\t\t</div>\n""")
 newHtmlFile.write("""\t\t\t<div class="row">\n""")
 newHtmlFile.write("""\t\t\t\t<table class="table table-borderless">\n""")
 newHtmlFile.write("""\t\t\t\t\t<thead>\n""")
-newHtmlFile.write("""\t\t\t\t\t<tr>\n""")
-newHtmlFile.write("""\t\t\t\t\t\t<th scope="col" style="border: 1px solid black;">Data: <a style="font-weight: lighter;">{{data}}</a></th>\n""")
-newHtmlFile.write("""\t\t\t\t\t\t<th scope="col"></th>\n""")
-newHtmlFile.write("""\t\t\t\t\t\t<th scope="col" style="border: 1px solid black;">Autista: <a style="font-weight: lighter;">{{autista}}</a></th>\n""")
-newHtmlFile.write("""\t\t\t\t\t</tr>\n""")
+newHtmlFile.write("""\t\t\t\t\t\t<tr>\n""")
+newHtmlFile.write("""\t\t\t\t\t\t\t<th scope="col" style="border: 1px solid black;">Data: <a style="font-weight: lighter;">{{data}}</a></th>\n""")
+newHtmlFile.write("""\t\t\t\t\t\t\t<th scope="col"></th>\n""")
+newHtmlFile.write("""\t\t\t\t\t\t\t<th scope="col" style="border: 1px solid black;">Autista: <a style="font-weight: lighter;">{{autista}}</a></th>\n""")
+newHtmlFile.write("""\t\t\t\t\t\t</tr>\n""")
 newHtmlFile.write("""\t\t\t\t\t</thead>\n""")
 newHtmlFile.write("""\t\t\t\t</table>\n""")
 newHtmlFile.write("""\t\t\t</div>\n""")
 newHtmlFile.write("""\t\t\t<div class="row">\n""")
 newHtmlFile.write("""\t\t\t\t<table class="table table-borderless">\n""")
 newHtmlFile.write("""\t\t\t\t\t<thead>\n""")
-newHtmlFile.write("""\t\t\t\t\t<tr>\n""")
-newHtmlFile.write("""\t\t\t\t\t\t<th scope='col' style='border: 1px solid black;'>Mittente</th>\n""")
-newHtmlFile.write("""\t\t\t\t\t\t<th scope='col' style='border: 1px solid black;'>Destinatario</th>\n""")
-newHtmlFile.write("""\t\t\t\t\t\t<th scope='col' style='border: 1px solid black;'>Via Destinatario</th>\n""")
-newHtmlFile.write("""\t\t\t\t\t\t<th scope='col' style='border: 1px solid black;'>Luogo</th>\n""")
-newHtmlFile.write("""\t\t\t\t\t\t<th scope='col' style='border: 1px solid black;'>Colli</th>\n""")
-newHtmlFile.write("""\t\t\t\t\t\t<th scope='col' style='border: 1px solid black;'>Peso</th>\n""")
-newHtmlFile.write("""\t\t\t\t\t\t<th scope='col' style='border: 1px solid black;'>Contrassegno</th>\n""")
-newHtmlFile.write("""\t\t\t\t\t</tr>\n""")
+newHtmlFile.write("""\t\t\t\t\t\t<tr>\n""")
+newHtmlFile.write("""\t\t\t\t\t\t\t<th scope='col' style='border: 1px solid black;'>Mittente</th>\n""")
+newHtmlFile.write("""\t\t\t\t\t\t\t<th scope='col' style='border: 1px solid black;'>Destinatario</th>\n""")
+newHtmlFile.write("""\t\t\t\t\t\t\t<th scope='col' style='border: 1px solid black;'>Via Destinatario</th>\n""")
+newHtmlFile.write("""\t\t\t\t\t\t\t<th scope='col' style='border: 1px solid black;'>Luogo</th>\n""")
+newHtmlFile.write("""\t\t\t\t\t\t\t<th scope='col' style='border: 1px solid black;'>Colli</th>\n""")
+newHtmlFile.write("""\t\t\t\t\t\t\t<th scope='col' style='border: 1px solid black;'>Peso</th>\n""")
+newHtmlFile.write("""\t\t\t\t\t\t\t<th scope='col' style='border: 1px solid black;'>Contrassegno</th>\n""")
+newHtmlFile.write("""\t\t\t\t\t\t</tr>\n""")
 newHtmlFile.write("""\t\t\t\t\t</thead>\n""")
 newHtmlFile.write("""\t\t\t\t\t<tbody>\n""")
 newHtmlFile.close()
@@ -81,11 +87,12 @@ newHtmlFile.close()
 # Inizializzo il dictionary di contesto del borderò
 context_bord = {}
 nome_corriere = ""
+doc_id, somma_contrassegni, somma_peso, somma_colli, somma_spedizioni = 0, 0, 0, 0, 0
+
 
 #
 # Genero un PDF per ogni spedizione
 #
-doc_id=0
 for i in lista_delle_spedizioni:
     causale = "Vendita"
     trasporto_a_mezzo = "Vettore"
@@ -122,19 +129,25 @@ for i in lista_delle_spedizioni:
     context_bord['peso_'+str(doc_id)] = peso
     context_bord['contrassegno_'+str(doc_id)] = contrassegno
 
+    somma_contrassegni+=int(contrassegno)
+    somma_spedizioni+=1
+    somma_peso +=int(peso)
+    somma_colli +=int(numero_colli)
+
     # Appendo all' HTML il placeholder
     newHtmlFile = open('templates/bordero.html', 'a')
-    newHtmlFile.write("""\t\t\t\t\t<tr>\n""")
-    newHtmlFile.write("""\t\t\t\t\t\t<td scope='col' style='border: 1px solid black;'><a style='font-weight: lighter;'>{{mitt_"""+str(doc_id)+"""}}</a></td>\n""")
-    newHtmlFile.write("""\t\t\t\t\t\t<td scope='col' style='border: 1px solid black;'><a style='font-weight: lighter;'>{{dest_"""+str(doc_id)+"""}}</a></td>\n""")
-    newHtmlFile.write("""\t\t\t\t\t\t<td scope='col' style='border: 1px solid black;'><a style='font-weight: lighter;'>{{viadest_"""+str(doc_id)+"""}}</a></td>\n""")
-    newHtmlFile.write("""\t\t\t\t\t\t<td scope='col' style='border: 1px solid black;'><a style='font-weight: lighter;'>{{luogo_"""+str(doc_id)+"""}}</a></td>\n""")
-    newHtmlFile.write("""\t\t\t\t\t\t<td scope='col' style='border: 1px solid black;'><a style='font-weight: lighter;'>{{colli_"""+str(doc_id)+"""}}</a></td>\n""")
-    newHtmlFile.write("""\t\t\t\t\t\t<td scope='col' style='border: 1px solid black;'><a style='font-weight: lighter;'>{{peso_"""+str(doc_id)+"""}}</a></td>\n""")
-    newHtmlFile.write("""\t\t\t\t\t\t<td scope='col' style='border: 1px solid black;'><a style='font-weight: lighter;'>{{contrassegno_"""+str(doc_id)+"""}}</a></td>\n""")
-    newHtmlFile.write("""\t\t\t\t\t<tr>\n""")
+    newHtmlFile.write("""\t\t\t\t\t\t<tr>\n""")
+    newHtmlFile.write("""\t\t\t\t\t\t\t<td scope='col' style='border: 1px solid black;'><a style='font-weight: lighter;'>{{mitt_"""+str(doc_id)+"""}}</a></td>\n""")
+    newHtmlFile.write("""\t\t\t\t\t\t\t<td scope='col' style='border: 1px solid black;'><a style='font-weight: lighter;'>{{dest_"""+str(doc_id)+"""}}</a></td>\n""")
+    newHtmlFile.write("""\t\t\t\t\t\t\t<td scope='col' style='border: 1px solid black;'><a style='font-weight: lighter;'>{{viadest_"""+str(doc_id)+"""}}</a></td>\n""")
+    newHtmlFile.write("""\t\t\t\t\t\t\t<td scope='col' style='border: 1px solid black;'><a style='font-weight: lighter;'>{{luogo_"""+str(doc_id)+"""}}</a></td>\n""")
+    newHtmlFile.write("""\t\t\t\t\t\t\t<td scope='col' style='border: 1px solid black;'><a style='font-weight: lighter;'>{{colli_"""+str(doc_id)+"""}}</a></td>\n""")
+    newHtmlFile.write("""\t\t\t\t\t\t\t<td scope='col' style='border: 1px solid black;'><a style='font-weight: lighter;'>{{peso_"""+str(doc_id)+"""}}</a></td>\n""")
+    newHtmlFile.write("""\t\t\t\t\t\t\t<td scope='col' style='border: 1px solid black;'><a style='font-weight: lighter;'>{{contrassegno_"""+str(doc_id)+"""}}</a></td>\n""")
+    newHtmlFile.write("""\t\t\t\t\t\t<tr>\n""")
     newHtmlFile.close()
 
+    # Genero il contesto passando il dictionary delle variabili dell'HTML
     context = {'logo':logo,'lettera_di_vettura': lettera_di_vettura, 'mittente_nome': mittente_nome, 
                 'mittente_via': mittente_via, 'mittente_luogo': mittente_luogo,
                 'destinatario_nome':destinatario_nome,'destinatario_via':destinatario_via,
@@ -154,14 +167,45 @@ for i in lista_delle_spedizioni:
     pdfkit.from_string(output_text, output_pdf, configuration=config, options=options)
     doc_id+=1
 
-# Ultimi rtag di chiusura dell'html
+# Ultimi tag di chiusura dell'html
 newHtmlFile = open('templates/bordero.html', 'a')
 newHtmlFile.write("""\t\t\t\t\t</tbody>\n""")
 newHtmlFile.write("""\t\t\t\t</table>\n""")
+newHtmlFile.write("""\t\t\t</div>\n""")
+newHtmlFile.write("""\t\t\t<div class="row">\n""")
+newHtmlFile.write("""\t\t\t\t<table class="table table-borderless">\n""")
+newHtmlFile.write("""\t\t\t\t\t<thead>\n""")
+newHtmlFile.write("""\t\t\t\t\t\t<tr>\n""")
+newHtmlFile.write("""\t\t\t\t\t\t\t<th scope='col' style='border: 1px solid black;'>Totale Spedizioni</th>\n""")
+newHtmlFile.write("""\t\t\t\t\t\t\t<th scope='col' style='border: 1px solid black;'>Totale Colli</th>\n""")
+newHtmlFile.write("""\t\t\t\t\t\t\t<th scope='col' style='border: 1px solid black;'>Totale Peso</th>\n""")
+newHtmlFile.write("""\t\t\t\t\t\t\t<th scope='col' style='border: 1px solid black;'>Totale Contrassegni</th>\n""")
+newHtmlFile.write("""\t\t\t\t\t\t\t<th scope='col' style='border: 1px solid black;'>Totale Consegne</th>\n""")
+newHtmlFile.write("""\t\t\t\t\t\t</tr>\n""")
+newHtmlFile.write("""\t\t\t\t\t</thead>\n""")
+newHtmlFile.write("""\t\t\t\t\t<tbody>\n""")
+newHtmlFile.write("""\t\t\t\t\t\t<tr>\n""")
+newHtmlFile.write("""\t\t\t\t\t\t\t<td scope='col' style='border: 1px solid black;'><a style='font-weight: lighter;'>{{somma_spedizioni}}</a></td>\n""")
+newHtmlFile.write("""\t\t\t\t\t\t\t<td scope='col' style='border: 1px solid black;'><a style='font-weight: lighter;'>{{somma_colli}}</a></td>\n""")
+newHtmlFile.write("""\t\t\t\t\t\t\t<td scope='col' style='border: 1px solid black;'><a style='font-weight: lighter;'>{{somma_peso}}</a></td>\n""")
+newHtmlFile.write("""\t\t\t\t\t\t\t<td scope='col' style='border: 1px solid black;'><a style='font-weight: lighter;'>{{somma_contrassegni}}</a></td>\n""")
+newHtmlFile.write("""\t\t\t\t\t\t\t<td scope='col' style='border: 1px solid black;'><a style='font-weight: lighter;'>{{numero_sped}}</a></td>\n""")
+newHtmlFile.write("""\t\t\t\t\t\t<tr>\n""")
+newHtmlFile.write("""\t\t\t\t\t</tbody>\n""")
+newHtmlFile.write("""\t\t\t\t</table>\n""")
+newHtmlFile.write("""\t\t\t</div>\n""")
 newHtmlFile.write("""\t\t</div>\n""")
-newHtmlFile.write("""\n\t</body>\n""")
+newHtmlFile.write("""\t</body>\n""")
 newHtmlFile.write("""</html>""")
 newHtmlFile.close()
+
+# Aggiungo i campi di riepilogo al context del bordero
+context_bord['somma_contrassegni'] = somma_contrassegni
+context_bord['somma_colli'] = somma_colli
+context_bord['somma_peso'] = somma_peso
+context_bord['somma_spedizioni'] = somma_spedizioni
+context_bord['numero_sped'] = numero_sped
+
 
 # Genero il borderò
 template_loader = jinja2.FileSystemLoader('/')
@@ -172,5 +216,3 @@ config = pdfkit.configuration(wkhtmltopdf=str_wkhtmltopdf)
 output_pdf = str_abs+'/output/'+'borderò_'+str(nome_corriere)+'_'+str(data_nome_pdf)+'.pdf'
 options={"enable-local-file-access": ""}
 pdfkit.from_string(output_text, output_pdf, configuration=config, options=options)
-
-print(context_bord)
--- src/ReadFromDb.py
+++ src/ReadFromDb.py
@@ -1,9 +1,12 @@
 lstt = []
+
 # DATI DI DEBUG #
 
-lstt.append(['445','DFG Servizi SRL','Via Corace 27', 'Catanzaro, 88100','LORY EXPRESS S.N.C.','Via Montepalma, 1','95125 Catania CT','150','Nessuna annotazione','4','40','19','27/10/2022','Pippo Calò','Pippo Calò'])
-lstt.append(['1990','DFG Servizi SRL','Via Corace 27', 'Catanzaro, 88100','Talmone Gift S.R.L','Via Jannelli, 15','Catanzaro 88100','470','Manca pacchetto','5','50','9981','27/10/2022','Pippo Calò','Pippo Calò'])
-lstt.append(['27110','DFG Servizi SRL','Via Corace 27','Catanzaro, 88100','Talmone Gift S.R.L','Via Jannelli, 15','Catanzaro 88100','470','Pacchetto trovato','1','100','881','27/10/2022','Pippo Calò','Pippo Calò'])
+numero_sped = 13
+
+lstt.append(['445','DFG Servizi SRL','Via Corace 27', 'Catanzaro, 88100','LORY EXPRESS S.N.C.','Via Montepalma, 1','95125 Catania CT','150','Nessuna annotazione','4','40','19','27/10/2022','Pippo Calò'])
+lstt.append(['1990','DFG Servizi SRL','Via Corace 27', 'Catanzaro, 88100','Talmone Gift S.R.L','Via Jannelli, 15','Catanzaro 88100','470','Manca pacchetto','5','50','9981','27/10/2022','Pippo Calò'])
+lstt.append(['27110','DFG Servizi SRL','Via Corace 27','Catanzaro, 88100','Talmone Gift S.R.L','Via Jannelli, 15','Catanzaro 88100','470','Pacchetto trovato','1','100','881','27/10/2022','Pippo Calò'])
 lstt.append(['444','DFG Servizi SRL','Via Corace 27','Catanzaro 88100','Coop SPA','Via Barone, 159','Catanzaro 88100','4500','-','10','110','34','27/10/2022','Pippo Calò'])
 lstt.append(['8188','DFG Servizi SRL','Via Corace 27', 'Catanzaro, 88100','LORY EXPRESS S.N.C.','Via Montepalma, 1','95125 Catania CT','150','Nessuna annotazione','4','40','19','27/10/2022','Pippo Calò'])
 lstt.append(['9109','DFG Servizi SRL','Via Corace 27', 'Catanzaro, 88100','Talmone Gift S.R.L','Via Jannelli, 15','Catanzaro 88100','470','Manca pacchetto','5','50','9981','27/10/2022','Pippo Calò'])
@@ -21,10 +24,14 @@ lstt.append(['2112313','DFG Servizi SRL','Via Corace 27', 'Catanzaro, 88100','LO
 lstt.append(['900','DFG Servizi SRL','Via Corace 27', 'Catanzaro, 88100','Talmone Gift S.R.L','Via Jannelli, 15','Catanzaro 88100','470','Manca pacchetto','5','50','9981','27/10/2022','Pippo Calò'])
 lstt.append(['6616','DFG Servizi SRL','Via Corace 27','Catanzaro, 88100','Talmone Gift S.R.L','Via Jannelli, 15','Catanzaro 88100','470','Pacchetto trovato','1','100','881','27/10/2022','Pippo Calò'])
 lstt.append(['7789','DFG Servizi SRL','Via Corace 27','Catanzaro 88100','Coop SPA','Via Barone, 159','Catanzaro 88100','4500','-','10','110','34','27/10/2022','Pippo Calò'])
+
 #
 # Far ritornare la lista dopo averla presa dal DB
 #
 def read_from_db():
     return lstt
+
+def numero_spedizioni():
+    return numero_sped
     
     
